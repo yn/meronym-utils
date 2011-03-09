511220c549d430655431257548f9e4a471514bbd
Yuri Niyazov <yuri.niyazov@gmail.com>
2011-03-08 23:04:14 -0800

    First commit. Copied clojure.contrib.condition out of clojure.contrib, made it meronym.condition and made it testable

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject meronym-utils "1.0.0"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :aot [meronym.condition.Condition])
