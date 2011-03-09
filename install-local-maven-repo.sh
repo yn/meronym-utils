dcddbf479a516cf933f47fd8995b327e269b5334
Yuri Niyazov <yuri.niyazov@gmail.com>
2011-03-09 00:48:29 -0800

    fixed version, readme, and install scripts

---
+++ install-local-maven-repo.sh
@@ -0,0 +1,9 @@
+mvn_install_plugin=org.apache.maven.plugins:maven-install-plugin:2.3.1
+mvn ${mvn_install_plugin}:install-file \
+-Dfile=meronym-utils-1.0.0-SNAPSHOT.jar \
+-DgroupId=meronym-utils \
+-DartifactId=meronym-utils \
+-Dversion=1.0.0-SNAPSHOT \
+-Dpackaging=jar \
+-DpomFile=pom.xml \
+-DcreateChecksum=true
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject meronym-utils "1.0.0"
+(defproject meronym-utils "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
