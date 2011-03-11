771c77d3aeaa7a068af964d16ec8401b2fc986e3
Yuri Niyazov <yuri.niyazov@gmail.com>
2011-03-10 18:33:31 -0800

    Added dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject meronym-utils "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "meronym utils"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :aot [meronym.condition.Condition])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.jsoup/jsoup "1.4.1"]
+                 [joda-time "1.6.2"]]
+  :aot [meronym.condition.Condition]
+  :dev-dependencies [[swank-clojure "1.2.1"]])
+
