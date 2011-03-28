0a27ea2c896052ffdbe2af96bbd7cc5f41c20047
Constantine Vetoshev <vetoshev@gmail.com>
2011-03-27 17:15:45 -0700

    Clojure 1.2.1.

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject meronym-utils "1.0.0-SNAPSHOT"
-  :description "meronym utils"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/data.json "0.1.0"]
                  [org.jsoup/jsoup "1.4.1"]
                  [joda-time "1.6.2"]]
   :aot [meronym.condition.Condition]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dev-dependencies [[swank-clojure "1.3.0"]])
 
